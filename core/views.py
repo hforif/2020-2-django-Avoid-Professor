3e8b038076175c299f34066a27107b6e8f96e994
Jung <jungeun.lak@gmail.com>
2020-11-13 11:33:04 +0900

    example 작성

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ItemForm
 
 
 # Create your views here.
@@ -8,3 +9,20 @@ def home(request):
     if request.user:
         ctx['user'] = request.user
     return render(request, 'core/home.html', ctx)
+
+
+# example : owner와 status를 지정하는 예시
+def example(request):
+    ctx = {}
+    if request.method == 'POST':
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.status = 'AC'
+            item.owner = request.user
+            item.save()
+        return redirect('home')
+    else:
+        form = ItemForm()
+        ctx['form'] = form
+    return render(request, 'core/sample.html', ctx)
